30b799361fb48497ec2547c696fe55e4da1ed234
joyshukla <shukla.joy@gmail.com>
2019-01-26 08:19:05 -0800

    install.sh logging

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
-# make hard links of each binary in /usr/local/bin
+echo 'make hard links of each binary in /usr/local/bin'
 
+
+
+echo sudo ln f /usr/local/bin/
 sudo ln f /usr/local/bin/
+
+echo sudo ln s /usr/local/bin/
 sudo ln s /usr/local/bin/
+
+echo sudo ln e /usr/local/bin/
 sudo ln e /usr/local/bin/
+
+echo sudo ln stack-operations /usr/local/bin/
 sudo ln stack-operations /usr/local/bin/
+
+echo
 
 echo 'jterm installed! enjoy the increased productivity! :-) '
