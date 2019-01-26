dc5f2f14c2293f74d42f9154bf5a588d32a2aa26
joyshukla <shukla.joy@gmail.com>
2019-01-26 08:13:35 -0800

    added TODO and install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# make hard links of each binary in /usr/local/bin
+
+sudo ln f /usr/local/bin/
+sudo ln s /usr/local/bin/
+sudo ln e /usr/local/bin/
+sudo ln stack-operations /usr/local/bin/
+
+echo 'jterm installed! enjoy the increased productivity! :-) '
